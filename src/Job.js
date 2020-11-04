2b2983e30990b7c0a2746968ae75d86f90074643
AsadullohR <58390694+AsadullohR@users.noreply.github.com>
2020-11-04 18:25:23 +0100

    Change card color

--- src/Job.js
+++ src/Job.js
@@ -7,20 +7,20 @@ export default function Job({ job }) {
 
   return (
       
-    <Card className="mb-3" color="#ffeb3b"> 
+    <Card className="mb-3 bg-secondary"> 
       <Card.Body>
         <div className="d-flex justify-content-between">
           <div>
-            <Card.Title>
-              {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
+            <Card.Title className="text-white">
+              {job.title} - <span className="text-light font-weight-light">{job.company}</span>
             </Card.Title>
-            <Card.Subtitle className="text-muted mb-2">
+            <Card.Subtitle className="text-light mb-2">
               {new Date(job.created_at).toLocaleDateString()}
             </Card.Subtitle>
-            <Badge variant="secondary" className="mr-2">{job.type}</Badge>
-            <Badge variant="secondary">{job.location}</Badge>
+            <Badge variant="success" className="mr-2">{job.type}</Badge>
+            <Badge variant="light">{job.location}</Badge>
             <div style={{ wordBreak: 'break-all' }}>
-              <ReactMarkdown source={job.how_to_apply} />
+              <ReactMarkdown className="text-light mt-3" source={job.how_to_apply} />
             </div>
           </div>
           <img className="d-none d-md-block" height="50" alt={job.company} src={job.company_logo} />
